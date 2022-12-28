5da3fcb27dcaff3ec5998dae5e2b041c513663a1
Samvel Bagdasaryan <samvelbagdasaryan1@gmail.com>
2022-12-29 00:33:22 +0300

    hw1 sty;es added

--- src/s2-homeworks/hw01/HW1.tsx
+++ src/s2-homeworks/hw01/HW1.tsx
@@ -34,7 +34,7 @@ export const message0: MessageType = {
         name: 'Sam',  // можно менять
     },
     message: {
-        text: 'some textsome textsome textsome textsome textsome textsome text', // можно менять
+        text: 'some textsome textsome textsome textsome  textsome', // можно менять
         time: '22:00', // можно менять
     },
 }
