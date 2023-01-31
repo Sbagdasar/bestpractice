edd3bdfbe85c5e673c158b4c2232a1935402e96c
Samvel Bagdasaryan <samvelbagdasaryan1@gmail.com>
2023-02-01 00:51:59 +0300

    feat:hw12 done

--- src/s2-homeworks/hw12/bll/themeReducer.ts
+++ src/s2-homeworks/hw12/bll/themeReducer.ts
@@ -1,14 +1,19 @@
 const initState = {
     themeId: 1,
 }
-
-export const themeReducer = (state = initState, action: any): any => { // fix any
+type InitStateType = typeof initState
+export const themeReducer = (state:InitStateType = initState, action: ChangeThemeIdType): InitStateType => { // fix any
     switch (action.type) {
         // дописать
-
+        case "SET_THEME_ID":{
+           return  {...state, themeId: action.id}
+        }
         default:
             return state
     }
 }
-
-export const changeThemeId = (id: number): any => ({ type: 'SET_THEME_ID', id }) // fix any
+type ChangeThemeIdType = {
+    type: 'SET_THEME_ID'
+    id:number
+}
+export const changeThemeId = (id: number): ChangeThemeIdType => ({ type: 'SET_THEME_ID', id }) // fix any
