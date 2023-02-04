d60d303ae7085bdf795e9acccde35b4463a9814b
Samvel Bagdasaryan <samvelbagdasaryan1@gmail.com>
2023-02-04 08:33:13 +0300

    fix:hw13 response messages

--- src/s2-homeworks/hw13/HW13.tsx
+++ src/s2-homeworks/hw13/HW13.tsx
@@ -37,8 +37,7 @@ const HW13 = () => {
                 console.log(res)
                 setCode('Код 200!')
                 setImage(success200)
-                setText('...всё ок)\n' +
-                    'код 200 - обычно означает что скорее всего всё ок)')
+                setText('...всё ок)')
                 setInfo('')
                 // дописать
 
@@ -48,23 +47,21 @@ const HW13 = () => {
                     case 500: {
                         setCode('Ошибка 500!')
                         setImage(error500)
-                        setText('эмитация ошибки на сервере ошибка 500 - обычно означает что что-то сломалось на сервере, например база данных')
+                        setText('эмитация ошибки на сервере')
                         setInfo('')
                         break
                     }
                     case 400: {
                         setCode('Ошибка 400!')
                         setImage(error400)
-                        setText('Ты не отправил success в body вообще!\n' +
-                            'ошибка 400 - обычно означает что скорее всего фронт отправил что-то не то на бэк!')
+                        setText('Ты не отправил success в body вообще!')
                         setInfo('')
                         break
                     }
                     default: {
                         setCode('Error')
                         setImage(errorUnknown)
-                        setText('Network Error\n' +
-                            'AxiosError')
+                        setText('Errork')
                         setInfo('')
                         break
                     }
