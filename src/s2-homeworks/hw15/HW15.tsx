a4b6d5e433771028cb5fbfdfe471c13fea08f02a
Samvel Bagdasaryan <samvelbagdasaryan1@gmail.com>
2023-02-06 00:23:20 +0300

    feat: hw15 first commit

--- src/s2-homeworks/hw15/HW15.tsx
+++ src/s2-homeworks/hw15/HW15.tsx
@@ -7,9 +7,9 @@ import {useSearchParams} from 'react-router-dom'
 import SuperSort from './common/c10-SuperSort/SuperSort'
 
 /*
-* 1 - дописать SuperPagination
-* 2 - дописать SuperSort
-* 3 - проверить pureChange тестами
+* 1 - дописать SuperPagination +
+* 2 - дописать SuperSort +
+* 3 - проверить pureChange тестами +
 * 3 - дописать sendQuery, onChangePagination, onChangeSort в HW15
 * 4 - сделать стили в соответствии с дизайном
 * 5 - добавить HW15 в HW5/pages/JuniorPlus
@@ -46,33 +46,33 @@ const HW15 = () => {
         getTechs(params)
             .then((res) => {
                 // делает студент
-
-                // сохранить пришедшие данные
-
-                //
+                if(res && res.data.techs.length > 0){
+                    setTotalCount(res.data.totalCount)
+                    setTechs(res.data.techs)
+                }
             })
     }
 
     const onChangePagination = (newPage: number, newCount: number) => {
         // делает студент
 
-        // setPage(
-        // setCount(
+        setPage(newPage)
+        setCount(newCount)
 
-        // sendQuery(
-        // setSearchParams(
+        sendQuery(searchParams)
+        setSearchParams({page:""+newPage, count:''+newCount})
 
         //
     }
 
     const onChangeSort = (newSort: string) => {
         // делает студент
+        setSort(newSort)
+        console.log(newSort + '-----')
+        setPage(1) // при сортировке сбрасывать на 1 страницу
 
-        // setSort(
-        // setPage(1) // при сортировке сбрасывать на 1 страницу
-
-        // sendQuery(
-        // setSearchParams(
+        sendQuery(searchParams)
+        setSearchParams({sort:""+newSort})
 
         //
     }
--- src/s2-homeworks/hw15/common/c10-SuperSort/SuperSort.tsx
+++ src/s2-homeworks/hw15/common/c10-SuperSort/SuperSort.tsx
@@ -13,8 +13,25 @@ export type SuperSortPropsType = {
 }
 
 export const pureChange = (sort: string, down: string, up: string) => {
-    // пишет студент, sort: (click) => down (click) => up (click) => '' (click) => down ...
-    return up // исправить
+    console.log(sort + '-sort-' + '-down-' + down + '-up-' + up)
+    if (sort === '') {
+        console.log(' down ')
+        return down
+    }
+    else if (sort === down) {
+        console.log(' - up')
+
+        return up
+    }
+    else if (sort === up) {
+        console.log(' - ')
+
+        return ''
+    }
+    else{
+        console.log('rrr')
+        return down
+    }
 }
 
 const SuperSort: React.FC<SuperSortPropsType> = (
@@ -24,7 +41,6 @@ const SuperSort: React.FC<SuperSortPropsType> = (
 ) => {
     const up = '0' + value
     const down = '1' + value
-
     const onChangeCallback = () => {
         onChange(pureChange(sort, down, up))
     }
@@ -40,13 +56,12 @@ const SuperSort: React.FC<SuperSortPropsType> = (
             id={id + '-sort-' + value}
             onClick={onChangeCallback}
         >
-            {/*сделать иконку*/}
-            {/*<img*/}
-            {/*    id={id + '-icon-' + sort}*/}
-            {/*    src={icon}*/}
-            {/*/>*/}
 
-            {icon} {/*а это убрать*/}
+            <img
+                id={id + '-icon-' + sort}
+                src={icon}
+            />
+
         </span>
     )
 }
